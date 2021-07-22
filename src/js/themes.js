776eade1ba830a792e589736e509eaef16ea5cf1
OlyaArta <artanika8@gmail.com>
2021-07-22 15:14:06 +0300

    added some things

---
+++ src/js/themes.js
@@ -0,0 +1,14 @@
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+//  const modalBox = document.querySelector('.lightbox');
+//  function onModalBOxOpen() {
+//  modalBox.classList.add('is-open');
+///  };
+  
+  // Закрытие модалки
+//  function onModalBoxClose(event) {
+ //   const modalBoxOpen = document.querySelector('.lightbox');
+ //   modalBoxOpen.classList.remove('is-open');
