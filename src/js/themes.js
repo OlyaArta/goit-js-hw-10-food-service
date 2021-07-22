c4f45fca66a84d40adb3c0b57a5e99d92cdc6826
OlyaArta <artanika8@gmail.com>
2021-07-22 17:55:40 +0300

    added new files

--- src/js/themes.js
+++ src/js/themes.js
@@ -3,6 +3,21 @@ const Theme = {
     DARK: 'dark-theme',
   };
 
+const themeSwitch = window.localStorage.getItem('DARK');
+const themeCheckBox = document.getElementById('theme-switch-toggle');
+themeCheckBox.checked = (themeSwitch === "true")? true : false;
+
+function myFunction() {
+   if(themeCheckBox.checked) {
+       document.documentElement.setAttribute('dark-theme', 'dark');
+       window.localStorage.setItem('dark-theme', true);
+   }
+   else {
+       document.documentElement.setAttribute('dark-theme', 'light');
+       window.localStorage.setItem('dark-theme', false);
+   }
+}
+
 //  const modalBox = document.querySelector('.lightbox');
 //  function onModalBOxOpen() {
 //  modalBox.classList.add('is-open');
